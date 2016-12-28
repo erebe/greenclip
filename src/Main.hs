395136e10546e7a078e3f5cd872bce19ff2e6306
Romain GERARD <r.gerard@criteo.com>
2016-12-28 17:10:46 +0100

    blabla

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,8 @@
-{-# LANGUAGE DeriveAnyClass     #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NoImplicitPrelude  #-}
 {-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE RecordWildCards    #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE TypeFamilies       #-}
 
 module Main where
 
@@ -13,49 +10,56 @@ import           ClassyPrelude    as CC
 import           Data.Binary      (Binary, decode, encode, get, put)
 import qualified Data.Text        as T
 import qualified Data.Vector      as V
+import           Lens.Micro.Mtl
+import           Lens.Micro.TH (makeLenses)
 import qualified System.Clipboard as Clip
 
 
 data Config = Config
-  { maxHistory               :: Int
-  , historyFilePath          :: String
-  , permanentHistoryFilePath :: String
-  }
+  { _maxHistoryLength  :: Int
+  , _historyPath       :: String
+  , _staticHistoryPath :: String
+  } deriving (Show, Read)
 
+makeLenses ''Config
 
 type ClipHistory = Vector Text
-
-instance Binary (Vector Text) where
-  put v = put (length v) >> foldMap put v
-  get = do
-    len <- get
-    history <- replicateM len get
-    return $ fromList history
+type CCC = ReaderT Config IO
 
 
+getHistory ::(MonadIO m, MonadReader Config m) => m (ClipHistory)
+getHistory = do
+  storePath <- view historyPath
+  history <- liftIO $ fromList . decode <$> readFile storePath
+  liftIO $ print history
+  return history
 
-startDaemon :: Config -> IO ()
-startDaemon Config{..} =
-  go (mempty :: ClipHistory)
-  where
-    go history = do
-      selection <- T.pack . fromMaybe mempty <$> Clip.getClipboardString
+storeHistory :: (MonadIO m, MonadReader Config m) => ClipHistory -> m ()
+storeHistory history = do
+  storePath <- view historyPath
+  liftIO $ writeFile storePath (encode . toList $ history)
 
-      let history' = appendToHistory selection history
-      _ <- writeToFile history'
+-- startDaemon :: Config -> IO ()
+-- startDaemon cfg@Config{..} =
+--   go (mempty :: ClipHistory)
+--   where
+--     go history = do
+--       selection <- T.pack . fromMaybe mempty <$> Clip.getClipboardString
 
-      _ <- threadDelay (10^(6 :: Int))
-      go history'
+--       history' <- appendToHistory selection history
 
-    appendToHistory selection h =
-      if selection /= fromMaybe mempty (headMay h)
-        then fst $ V.splitAt maxHistory $ cons selection h
-        else h
+--       _ <- threadDelay (10^(6 :: Int))
+--       go history'
 
-    writeToFile history = do
-      writeFile historyFilePath (encode history)
+--     appendToHistory selection h =
+--       if selection /= fromMaybe mempty (headMay h)
+--         then do
+--           let history' = fst . V.splitAt maxHistoryLength $ cons selection h
+--           _ <- storeHistory cfg history'
+--           return history'
+--         else return h
 
 main :: IO ()
 main = do
-  -- h <- decode <$> readFile "history.svg" :: IO (Vector Text)
-  startDaemon (Config 5 "history.svg" mempty)
+  -- startDaemon (Config 5 "history.svg" mempty)
+  undefined
