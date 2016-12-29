2f26e6dff9f15e7977452b3763806778a51f0d88
Erèbe <romain.gerard@erebe.eu>
2016-12-29 18:17:49 +0100

    Working version

--- src/Main.hs
+++ src/Main.hs
@@ -1,65 +1,124 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE NoImplicitPrelude  #-}
-{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 module Main where
 
-import           ClassyPrelude    as CC
-import           Data.Binary      (Binary, decode, encode, get, put)
+import           ClassyPrelude    as CP
+import           Data.Binary      (decode, encode)
+import           Data.List        (nub)
 import qualified Data.Text        as T
 import qualified Data.Vector      as V
+import           Lens.Micro
 import           Lens.Micro.Mtl
-import           Lens.Micro.TH (makeLenses)
 import qualified System.Clipboard as Clip
+import qualified System.Directory as Dir
 
+
+data Command = DAEMON | PRINT | COPY Text | HELP deriving (Show, Read)
 
 data Config = Config
-  { _maxHistoryLength  :: Int
-  , _historyPath       :: String
-  , _staticHistoryPath :: String
+  { maxHistoryLength  :: Int
+  , historyPath       :: String
+  , staticHistoryPath :: String
   } deriving (Show, Read)
 
-makeLenses ''Config
 
 type ClipHistory = Vector Text
 type CCC = ReaderT Config IO
 
 
-getHistory ::(MonadIO m, MonadReader Config m) => m (ClipHistory)
+getHistory :: (MonadIO m, MonadReader Config m) => m ClipHistory
 getHistory = do
-  storePath <- view historyPath
-  history <- liftIO $ fromList . decode <$> readFile storePath
-  liftIO $ print history
-  return history
+  storePath <- view (to historyPath)
+  liftIO $ readH storePath
+  where
+    readH filePath = (readFile filePath <&> fromList . decode) `catchAnyDeep` (const mempty)
+
+getStaticHistory :: (MonadIO m, MonadReader Config m) => m ClipHistory
+getStaticHistory = do
+  storePath <- view (to staticHistoryPath)
+  liftIO $ readH storePath
+  where
+    readH filePath = (readFile filePath <&> fromList . T.lines) `catchAnyDeep` (const mempty)
 
 storeHistory :: (MonadIO m, MonadReader Config m) => ClipHistory -> m ()
 storeHistory history = do
-  storePath <- view historyPath
-  liftIO $ writeFile storePath (encode . toList $ history)
+  storePath <- view (to historyPath)
+  liftIO $ writeFile storePath (encode . toList $ history) `catchAnyDeep` (const (return ()))
 
--- startDaemon :: Config -> IO ()
--- startDaemon cfg@Config{..} =
---   go (mempty :: ClipHistory)
---   where
---     go history = do
---       selection <- T.pack . fromMaybe mempty <$> Clip.getClipboardString
+appendH :: (MonadIO m, MonadReader Config m) => Text -> ClipHistory -> m (ClipHistory)
+appendH sel history =
+  let selection = T.strip sel in
+  if selection == mempty
+     || selection == fromMaybe mempty (headMay history)
+  then return $ history
+  else do
+    maxLen <- view (to maxHistoryLength)
+    return $ fst . V.splitAt maxLen . fromList . nub . toList $ cons selection history
 
---       history' <- appendToHistory selection history
+runDaemon :: (MonadIO m, MonadReader Config m) => m ()
+runDaemon = getHistory >>= go
+  where
+    _1sec :: Int
+    _1sec = 10^6
 
---       _ <- threadDelay (10^(6 :: Int))
---       go history'
+    go history = do
+      selection <- liftIO $ getSelection
 
---     appendToHistory selection h =
---       if selection /= fromMaybe mempty (headMay h)
---         then do
---           let history' = fst . V.splitAt maxHistoryLength $ cons selection h
---           _ <- storeHistory cfg history'
---           return history'
---         else return h
+      history' <- appendH selection history
+      when (history' /= history) (storeHistory history')
+
+      liftIO $ threadDelay (10^(6 :: Int))
+      go history'
+
+    getSelection :: IO Text
+    getSelection = T.pack . fromMaybe mempty <$> Clip.getClipboardString
+
+printHistory :: (MonadIO m, MonadReader Config m) => m ()
+printHistory = do
+  history <- mappend <$> getHistory <*> getStaticHistory
+  traverse print history
+  return ()
+
+
+getConfig :: IO Config
+getConfig = do
+  home <- Dir.getHomeDirectory
+  let cfgPath = home </> ".config/mclip.cfg"
+
+  cfgStr <- readFile cfgPath `catchAnyDeep` (const mempty) :: IO Text
+  let cfg = fromMaybe (defaultConfig home) (readMay cfgStr)
+
+  writeFile cfgPath (show cfg)
+  return cfg
+
+  where
+    defaultConfig home = Config 25 (home </> ".cache/mclip.history") (home </> ".cache/mclip.staticHistory")
+
+pasteSelection :: Text -> IO ()
+pasteSelection sel = Clip.setClipboardString (fromMaybe mempty (readMay sel))
+
+parseArgs :: [Text] -> Command
+parseArgs ("daemon":_)     = DAEMON
+parseArgs ("print":[])     = PRINT
+parseArgs ("print":sel:[]) = COPY sel
+parseArgs _                = HELP
+
+run :: Command -> IO ()
+run cmd = do
+  cfg <- getConfig
+  case cmd of
+    DAEMON   -> runReaderT runDaemon cfg
+    PRINT    -> runReaderT printHistory cfg
+    COPY sel -> pasteSelection sel
+    HELP     -> print "HELP"
 
 main :: IO ()
 main = do
-  -- startDaemon (Config 5 "history.svg" mempty)
-  undefined
+  cmd <- parseArgs <$> getArgs
+  run cmd
