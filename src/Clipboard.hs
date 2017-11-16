a6f9175f54c4c69dd07f65a9eb577ba8157d3003
Erèbe <romain.gerard@erebe.eu>
2017-11-16 23:34:06 +0100

    Rework for 3.0

--- src/Clipboard.hs
+++ src/Clipboard.hs
@@ -1,119 +1,276 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RecordWildCards       #-}
 
-module Clipboard
-  ( getClipboardString
-  , setClipboardString
-  , getPrimaryClipboard
-  ) where
+module Clipboard where
 
-import Graphics.X11.Xlib
-import Graphics.X11.Xlib.Extras
-import System.Posix.Process     (forkProcess)
+import           Protolude
 
-import Codec.Binary.UTF8.String (decode, encode)
-import Control.Monad
-import Data.Maybe
-import Foreign                  (peekByteOff)
-import Foreign.C.Types          (CChar, CUChar)
-import Foreign.Marshal.Array    (withArrayLen)
-import System.Directory         (setCurrentDirectory)
-import System.IO                (hClose, stderr, stdin, stdout)
-import System.IO.Unsafe (unsafePerformIO)
-import Data.IORef
-import Control.Concurrent (threadDelay)
+import           Graphics.X11.Xlib
+import           Graphics.X11.Xlib.Extras
 
-getClipboardString :: IO (Maybe String)
-getClipboardString = do
-    (display, window, clipboards) <- readIORef initialSetup
-    inp <- internAtom display "clipboard_get" False
-    target <- internAtom display "UTF8_STRING" True
-    xConvertSelection display (head clipboards) target inp window currentTime
-    Just <$> clipboardInputWait display window inp
+import           Lens.Micro               ((<&>))
+import qualified Data.ByteString as B
+import           Control.Concurrent       (threadDelay)
 
-getPrimaryClipboard :: IO (Maybe String)
-getPrimaryClipboard = do
-    (display, window, clipboards) <- readIORef initialSetup
-    inp <- internAtom display "clipboard_get" False
-    target <- internAtom display "UTF8_STRING" True
-    xConvertSelection display (clipboards !! 1) target inp window currentTime
-    Just <$> clipboardInputWait display window inp
+-- import           System.Posix.Process     (forkProcess)
+-- import           System.Directory         (setCurrentDirectory)
+-- import           System.IO                (hClose, stderr, stdin, stdout)
 
-clipboardInputWait :: Display -> Window -> Atom -> IO String
-clipboardInputWait display' window' inp' = allocaXEvent (go display' window' inp')
+-- import           Foreign.C.Types          (CChar, CUChar)
+-- import           Foreign.Marshal.Array    (withArrayLen)
+import           Foreign                  (alloca, castPtr, peek,
+                                           peekArray, {- peekByteOff -})
+
+data SelectionType = UTF8 Text
+                   | PNG ByteString
+                   | JPEG ByteString
+                   | BITMAP ByteString
+                   deriving (Show)
+
+data Selection = Selection {
+    appName   :: Text
+  , selection :: SelectionType
+} deriving (Show)
+
+data XorgContext = XorgContext {
+    display          :: Display
+  , ownWindow        :: Window
+  , defaultClipboard :: Atom
+  , primaryClipboard :: Atom
+  , selectionTarget  :: Atom
+} deriving (Show)
+
+
+test :: IO ()
+test = do
+  ctx <- getXorgContext
+
+  ret <- getClipboardSelection ctx
+  print $! ret
+
+  return ()
+
+windowNameOfClipboardOwner :: XorgContext -> Atom -> IO Text
+windowNameOfClipboardOwner XorgContext{..} clipboard = do
+
+  window <- xGetSelectionOwner display clipboard
+  windowName <- fetchName display window <&> toS . fromMaybe mempty
+  return $! windowName
+
+
+getSupportedMimes :: XorgContext -> Atom -> IO [Text]
+getSupportedMimes ctx@XorgContext{..} clipboard =
+    alloca $ \actual_type_return ->
+    alloca $ \actual_format_return ->
+    alloca $ \nitems_return ->
+    alloca $ \bytes_after_return ->
+    alloca $ \prop_return -> do
+        targets <- internAtom display "TARGETS" False
+        xConvertSelection display clipboard targets selectionTarget ownWindow currentTime
+        _ <- waitNotify ctx
+        ret <- xGetWindowProperty display ownWindow selectionTarget 0 0xFFFFFFFF False aTOM
+                           actual_type_return
+                           actual_format_return
+                           nitems_return
+                           bytes_after_return
+                           prop_return
+
+        ret2 <- if ret /= 0
+                then return Nothing
+                else do
+                    prop_ptr      <- peek prop_return
+                    actual_format <- peek actual_format_return <&> fromIntegral :: IO Atom
+                    nitems        <- peek nitems_return <&> fromIntegral
+                    getprop prop_ptr nitems actual_format
+        print $ ret2
+        fmap (fmap toS) (getAtomNames display $ fromMaybe mempty ret2)
   where
-  go display window inp evPtr = do
-    waitForEvent display
-    nextEvent display evPtr
-    ev <- getEvent evPtr
-    if ev_event_type ev == selectionNotify
-       then charsToString . fromMaybe mempty <$> getWindowProperty8 display inp window
-       else go display window inp evPtr
-  waitForEvent display = do
-    nbEvs <- pending display
-    when (nbEvs == 0) $ threadDelay 100000 >> waitForEvent display
+    getprop prop_ptr nitems actual_format
+        | actual_format == 0    = return Nothing -- Property not found
+        | otherwise = do
+            retval <- peekArray nitems (castPtr prop_ptr)
+            _ <- xFree prop_ptr
+            return $ Just retval
 
-charsToString :: [CChar] -> String
-charsToString = decode . map fromIntegral
 
-setClipboardString :: String -> IO ()
-setClipboardString str = void $ forkProcess $ do
-        mapM_ hClose [stdin, stdout, stderr]
-        setCurrentDirectory "/"
-        (display, window, clipboards) <- readIORef initialSetup
-        mapM_ (\atom -> xSetSelectionOwner display atom window currentTime) clipboards
-        advertiseSelection display clipboards (stringToChars str)
-        cleanup display window
+getClipboardSelection :: XorgContext -> IO Selection
+getClipboardSelection ctx@XorgContext{..} =
+  getSelection ctx defaultClipboard
 
-advertiseSelection :: Display -> [Atom] -> [CUChar] -> IO ()
-advertiseSelection display clipboards' str = allocaXEvent (go clipboards')
-  where
-    go [] _ = return ()
-    go clipboards evPtr = do
-      nextEvent display evPtr
-      ev <- getEvent evPtr
-      case ev of
-          SelectionRequest {..} -> do
-              target' <- getAtomName display ev_target
-              res <- handleOutput display ev_requestor ev_property target' str
-              sendSelectionNotify display ev_requestor ev_selection ev_target res ev_time
-              go clipboards evPtr
+getPrimarySelection :: XorgContext -> IO Selection
+getPrimarySelection ctx@XorgContext{..} =
+  getSelection ctx primaryClipboard
 
-          _ | ev_event_type ev == selectionClear -> do
-              target <- peekByteOff evPtr 40 :: IO Atom
-              go (filter (/= target) clipboards) evPtr
+getSelection :: XorgContext -> Atom -> IO Selection
+getSelection ctx@XorgContext{..} clipboard = do
+  mimes <- getSupportedMimes ctx clipboard
+  print mimes
+  let selectedMime = chooseSelectionType mimes
 
-          _ -> go clipboards evPtr
+  target <- internAtom display (toS selectedMime) True
+  xConvertSelection display clipboard target selectionTarget ownWindow currentTime
+  waitNotify ctx
 
-handleOutput :: Display -> Window -> Atom -> Maybe String -> [CUChar] -> IO Atom
-handleOutput display req prop (Just "UTF8_STRING") str = do
-    prop' <- getAtomName display prop
-    if isNothing prop' then handleOutput display req prop Nothing str else do
-        target <- internAtom display "UTF8_STRING" True
-        void $ withArrayLen str $ \len str' ->
-            xChangeProperty display req prop target 8 propModeReplace str'
-                            (fromIntegral len)
-        return prop
-handleOutput _ _ _ _ _ = return none
+  clipboardContent <- getWindowProperty8 display selectionTarget ownWindow
+                      <&> B.pack . map fromIntegral . fromMaybe mempty
 
-sendSelectionNotify :: Display -> Window -> Atom -> Atom -> Atom -> Time -> IO ()
-sendSelectionNotify display req sel target prop time = allocaXEvent $ \ev -> do
-    setEventType ev selectionNotify
-    setSelectionNotify ev req sel target prop time
-    sendEvent display req False 0 ev
+  windowName <- windowNameOfClipboardOwner ctx clipboard
+  return Selection {
+    appName = windowName
+  , selection = mimeToSelectionType selectedMime clipboardContent
+  }
 
-stringToChars :: String -> [CUChar]
-stringToChars = map fromIntegral . encode
+ where
+   priorities = ["image/png", "image/jpeg", "image/bmp", "UTF8_STRING", "TEXT"]
 
-initialSetup :: IORef (Display, Window, [Atom])
-initialSetup = unsafePerformIO $ do
-    display <- openDisplay ""
-    window <- createSimpleWindow display (defaultRootWindow display)
-                                 0 0 1 1 0 0 0
-    clipboards <- internAtom display "CLIPBOARD" False
-    newIORef (display, window, [clipboards, pRIMARY])
-{-# NOINLINE initialSetup #-}
+   chooseSelectionType mimes =
+     let selectedMime = msum $ (\mime -> find (== mime) mimes) <$> priorities
+     in fromMaybe "UTF8_STRING" selectedMime
 
-cleanup :: Display -> Window -> IO ()
-cleanup display window = do
-    destroyWindow display window
+   mimeToSelectionType "image/png" selContent = PNG selContent
+   mimeToSelectionType "image/jpeg" selContent = JPEG selContent
+   mimeToSelectionType "image/bmp" selContent = BITMAP selContent
+   mimeToSelectionType _ selContent = UTF8 (toS selContent)
+
+
+getXorgContext :: IO XorgContext
+getXorgContext = do
+    display <- openDisplay mempty
+    window <- createSimpleWindow display (defaultRootWindow display) 0 0 1 1 0 0 0
+    clipboard <- internAtom display "CLIPBOARD" False
+    selTarget <- internAtom display "GREENCLIP" False
+    return XorgContext {
+        display = display
+      , ownWindow = window
+      , defaultClipboard = clipboard
+      , primaryClipboard = pRIMARY
+      , selectionTarget = selTarget
+    }
+
+destroyXorgContext :: XorgContext -> IO ()
+destroyXorgContext XorgContext{..} = do
+    destroyWindow display ownWindow
     closeDisplay display
+
+waitNotify :: XorgContext -> IO ()
+waitNotify XorgContext{..} = allocaXEvent (go display ownWindow)
+  where
+  go display' window evPtr = do
+    waitForEvents display'
+    nextEvent display' evPtr
+    ev <- getEvent evPtr
+    when (ev_event_type ev /= selectionNotify)
+      (go display' window evPtr)
+
+  waitForEvents display' = do
+    nbEvs <- pending display'
+    when (nbEvs == 0) $ threadDelay _1ms >> waitForEvents display'
+
+  _1ms = 100000
+
+
+
+
+
+
+
+
+-- getClipboardString :: IO (Maybe Text)
+-- getClipboardString = do
+--     (display, window, clipboards) <- readIORef initialSetup
+--
+--     inp <- internAtom display "clipboard_get" False
+--     target <- internAtom display "UTF8_STRING" True
+--     xConvertSelection display (unsafeHead clipboards) target inp window currentTime
+--     Just <$> waitClipboardNotify display window inp
+--
+-- getPrimaryClipboard :: IO (Maybe Text)
+-- getPrimaryClipboard = do
+--     (display, window, clipboards) <- readIORef initialSetup
+--     inp <- internAtom display "clipboard_get" False
+--     target <- internAtom display "UTF8_STRING" True
+--     xConvertSelection display (unsafeIndex clipboards 1) target inp window currentTime
+--     Just <$> waitClipboardNotify display window inp
+--
+-- waitClipboardNotify :: Display -> Window -> Atom -> IO Text
+-- waitClipboardNotify display' window' inp' = allocaXEvent (go display' window' inp')
+--   where
+--   go display window inp evPtr = do
+--     waitForEvent display
+--     nextEvent display evPtr
+--     ev <- getEvent evPtr
+--     if ev_event_type ev == selectionNotify
+--        then charsToString . fromMaybe mempty <$> getWindowProperty8 display inp window
+--        else go display window inp evPtr
+--   waitForEvent display = do
+--     nbEvs <- pending display
+--     when (nbEvs == 0) $ threadDelay 100000 >> waitForEvent display
+--
+-- charsToString :: [CChar] -> Text
+-- charsToString = toS . decode . map fromIntegral
+--
+-- setClipboardString :: Text -> IO ()
+-- setClipboardString str = void $ forkProcess $ do
+--         mapM_ hClose [stdin, stdout, stderr]
+--         setCurrentDirectory "/"
+--         (display, window, clipboards) <- readIORef initialSetup
+--         mapM_ (\atom -> xSetSelectionOwner display atom window currentTime) clipboards
+--         advertiseSelection display clipboards (stringToChars str)
+--         cleanup display window
+--
+-- advertiseSelection :: Display -> [Atom] -> [CUChar] -> IO ()
+-- advertiseSelection display clipboards' str = allocaXEvent (go clipboards')
+--   where
+--     go [] _ = return ()
+--     go clipboards evPtr = do
+--       nextEvent display evPtr
+--       ev <- getEvent evPtr
+--       case ev of
+--           SelectionRequest {..} -> do
+--               target' <- getAtomName display ev_target
+--               res <- handleOutput display ev_requestor ev_property (toS <$> target') str
+--               sendSelectionNotify display ev_requestor ev_selection ev_target res ev_time
+--               go clipboards evPtr
+--
+--           _ | ev_event_type ev == selectionClear -> do
+--               target <- peekByteOff evPtr 40 :: IO Atom
+--               go (filter (/= target) clipboards) evPtr
+--
+--           _ -> go clipboards evPtr
+--
+-- handleOutput :: Display -> Window -> Atom -> Maybe Text -> [CUChar] -> IO Atom
+-- handleOutput display req prop (Just "UTF8_STRING") str = do
+--     prop' <- getAtomName display prop
+--     if isNothing prop' then handleOutput display req prop Nothing str else do
+--         target <- internAtom display "UTF8_STRING" True
+--         void $ withArrayLen str $ \len str' ->
+--             xChangeProperty display req prop target 8 propModeReplace str'
+--                             (fromIntegral len)
+--         return prop
+-- handleOutput _ _ _ _ _ = return none
+--
+-- sendSelectionNotify :: Display -> Window -> Atom -> Atom -> Atom -> Time -> IO ()
+-- sendSelectionNotify display req sel target prop time = allocaXEvent $ \ev -> do
+--     setEventType ev selectionNotify
+--     setSelectionNotify ev req sel target prop time
+--     sendEvent display req False 0 ev
+--
+-- stringToChars :: Text -> [CUChar]
+-- stringToChars = map fromIntegral . encode . toS
+--
+-- initialSetup :: IORef (Display, Window, [Atom])
+-- initialSetup = unsafePerformIO $ do
+--     display <- openDisplay ""
+--     window <- createSimpleWindow display (defaultRootWindow display)
+--                                  0 0 1 1 0 0 0
+--     clipboards <- internAtom display "CLIPBOARD" False
+--     newIORef (display, window, [clipboards, pRIMARY])
+-- {-# NOINLINE initialSetup #-}
+--
+--
+-- cleanup :: Display -> Window -> IO ()
+-- cleanup display window = do
+--     destroyWindow display window
+--     closeDisplay display
--- src/Main.hs
+++ src/Main.hs
@@ -1,33 +1,37 @@
-{-# LANGUAGE BangPatterns          #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NoImplicitPrelude     #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeFamilies          #-}
+
 
 module Main where
 
-import           ClassyPrelude      as CP hiding (readFile)
-import           Data.Binary        (decode, encode)
-import qualified Data.Text          as T
-import qualified Data.Vector        as V
+import           Protolude             hiding (readFile, to)
+
+import           Control.Monad.Catch   (MonadCatch, catchAll)
+import           Data.Binary           (decode, encode)
+import qualified Data.Text             as T
+import           Data.Vector           (Vector)
+import qualified Data.Vector           as V
 import           Lens.Micro
 import           Lens.Micro.Mtl
-import qualified Clipboard   as Clip
-import qualified System.Directory   as Dir
-import           System.Environment (lookupEnv)
-import           System.IO          (IOMode (..), openFile)
-import           System.Timeout     (timeout)
-import           System.Wordexp.Simple
+import qualified System.Directory      as Dir
+import           System.Environment    (lookupEnv)
+import           System.IO             (IOMode (..), hClose, hGetContents,
+                                        openFile)
+import           System.Timeout        (timeout)
+import           System.Wordexp.Simple (wordexp)
+
+import qualified Clipboard             as Clip
 
 
 data Command = DAEMON | PRINT | COPY Text | CLEAR | HELP deriving (Show, Read)
 
 data Config = Config
   { maxHistoryLength           :: Int
-  , historyPath                :: String
-  , staticHistoryPath          :: String
+  , historyPath                :: Text
+  , staticHistoryPath          :: Text
   , usePrimarySelectionAsInput :: Bool
   } deriving (Show, Read)
 
@@ -36,52 +40,60 @@ type ClipHistory = Vector Text
 readFile :: FilePath -> IO ByteString
 readFile filepath = bracket (openFile filepath ReadMode) hClose $ \h -> do
   str <- hGetContents h
-  let !str' = str
-  return str'
+  return $! toS str
 
 
 getHistory :: (MonadIO m, MonadReader Config m) => m ClipHistory
 getHistory = do
-  storePath <- view (to historyPath)
-  liftIO $ readH storePath `catchAnyDeep` const mempty
+  storePath <- view $ to (toS . historyPath)
+  liftIO $ readH storePath
   where
-    readH filePath = readFile filePath <&> fromList . decode . fromStrict
+    readH filePath = readFile filePath <&> V.fromList . decode . toS
+
 
 getStaticHistory :: (MonadIO m, MonadReader Config m) => m ClipHistory
 getStaticHistory = do
-  storePath <- view (to staticHistoryPath)
-  liftIO $ readH storePath `catchAnyDeep` const mempty
+  storePath <- view $ to (toS . staticHistoryPath)
+  liftIO $ readH storePath
   where
-    readH filePath = readFile filePath <&> fromList . T.lines . decodeUtf8
+    readH filePath = readFile filePath <&> V.fromList . T.lines . toS
 
 
 storeHistory :: (MonadIO m, MonadReader Config m) => ClipHistory -> m ()
 storeHistory history = do
-  storePath <- view (to historyPath)
-  liftIO $ writeH storePath history `catchAnyDeep` const mempty
+  storePath <- view $ to (toS . historyPath)
+  liftIO $ writeH storePath history
   where
-    writeH storePath = writeFile storePath . toStrict . encode . toList
+    writeH storePath = writeFile storePath . toS . encode . V.toList
+
 
 appendToHistory :: (MonadIO m, MonadReader Config m) => Text -> ClipHistory -> m ClipHistory
 appendToHistory sel history =
   let selection = T.strip sel in
-  if selection == mempty || selection == fromMaybe mempty (headMay history)
+  if selection == mempty || selection == fromMaybe mempty (V.headM history)
   then return history
   else do
-    maxLen <- view (to maxHistoryLength)
-    return $ fst . V.splitAt maxLen . cons selection $ filter (/= selection) history
+    maxLen <- view $ to maxHistoryLength
+    return $ fst . V.splitAt maxLen . V.cons selection $ V.filter (/= selection) history
 
 
-runDaemon :: (MonadIO m, MonadReader Config m, MonadCatch m) => m ()
-runDaemon = do
-  usePrimary <- view (to usePrimarySelectionAsInput)
-  let getSelections = (getSelectionFrom Clip.getClipboardString, mempty)
-                    : [(getSelectionFrom Clip.getPrimaryClipboard, mempty) | usePrimary]
-  forever $ (getHistory >>= go getSelections) `catchAnyDeep` handleError
+runDaemon :: (MonadIO m, MonadCatch m, MonadReader Config m) => m ()
+runDaemon = forever $ run `catchAll` handleError
 
   where
     _0_5sec :: Int
-    _0_5sec = 5 * 10^(5::Int)
+    _0_5sec = 5 * 100000
+
+    _1sec :: Int
+    _1sec = 1000000
+
+    run = do
+      history <- getHistory
+      x11Context <- liftIO Clip.getXorgContext
+      usePrimary <- view $ to usePrimarySelectionAsInput
+      let getSelections = (getSelectionFrom (Clip.getClipboardSelection x11Context), mempty)
+                        : [(getSelectionFrom (Clip.getPrimarySelection x11Context), mempty) | usePrimary]
+      go getSelections history
 
     getSelection [] = return ([], mempty)
     getSelection ((getSel, lastSel):getSels) = do
@@ -98,13 +110,18 @@ runDaemon = do
       liftIO $ threadDelay _0_5sec
       go getSelections' history'
 
-    getSelectionFrom fn = timeout 1000000 fn <&> T.pack . fromMaybe mempty . join
+    getSelectionFrom fn = do
+      sel <- timeout _1sec fn
+      case sel of
+        Nothing -> return mempty
+        Just _  -> return "ok"
 
     handleError ex = do
-      let displayMissing = "openDisplay" `T.isInfixOf` tshow ex
+      let displayMissing = "openDisplay" `T.isInfixOf` show ex
       if displayMissing
-      then error "X display not available. Please start Xorg before running greenclip"
-      else sayErrShow ex
+      then panic "X display not available. Please start Xorg before running greenclip"
+      else print ex
+      liftIO $ threadDelay _0_5sec
 
 
 toRofiStr :: Text -> Text
@@ -113,33 +130,36 @@ toRofiStr = T.map (\c -> if c == '\n' || c == '\r' then '\xA0' else c)
 fromRofiStr :: Text -> Text
 fromRofiStr = T.map (\c -> if c == '\xA0' then '\n' else c)
 
+
 printHistoryForRofi :: (MonadIO m, MonadReader Config m) => m ()
 printHistoryForRofi = do
   history <- mappend <$> getHistory <*> getStaticHistory
   _ <- traverse (putStrLn . toRofiStr) history
   return ()
 
+
 advertiseSelection :: Text -> IO ()
-advertiseSelection = Clip.setClipboardString . T.unpack . fromRofiStr
+advertiseSelection = undefined -- Clip.setClipboardString . fromRofiStr
+
 
 getConfig :: IO Config
 getConfig = do
   home <- Dir.getHomeDirectory
-  let cfgPath = home </> ".config/greenclip.cfg"
+  let cfgPath = home <> "/.config/greenclip.cfg"
 
-  cfgStr <- (readFile cfgPath <&> T.strip . decodeUtf8) `catchAnyDeep` const mempty
-  let cfg = fromMaybe (defaultConfig home) (readMay cfgStr)
-  writeFile cfgPath (fromString $ show cfg)
+  cfgStr <- (readFile cfgPath <&> T.strip . toS) `catchAll` const mempty
+  let cfg = fromMaybe (defaultConfig (toS home)) (readMaybe $ toS cfgStr)
+  writeFile cfgPath (show cfg)
 
-  historyPath' <- expandHomeDir $ historyPath cfg
-  staticHistoryPath' <- expandHomeDir $ staticHistoryPath cfg
+  historyPath' <- expandHomeDir . toS $ historyPath cfg
+  staticHistoryPath' <- expandHomeDir . toS $ staticHistoryPath cfg
 
-  let cfg' = cfg {historyPath = historyPath', staticHistoryPath = staticHistoryPath'}
+  let cfg' = cfg {historyPath = toS historyPath', staticHistoryPath = toS staticHistoryPath'}
   return cfg'
 
   where
-    defaultConfig home = Config 25 (home </> ".cache/greenclip.history") (home </> ".cache/greenclip.staticHistory") False
-    expandHomeDir str = (fromMaybe str . listToMaybe <$> wordexp str) `catch` (\(_ :: SomeException) -> return str)
+    defaultConfig home = Config 25 (home <> "/.cache/greenclip.history") (home <> "/.cache/greenclip.staticHistory") False
+    expandHomeDir str = (fromMaybe str . listToMaybe <$> wordexp str) `catchAll` (\_ -> return str)
 
 
 parseArgs :: [Text] -> Command
@@ -159,16 +179,16 @@ run cmd = do
     -- Should rename COPY into ADVERTISE but as greenclip is already used I don't want to break configs
     -- of other people
     COPY sel -> advertiseSelection sel
-    HELP     -> putStrLn $ "greenclip v2.1 -- Recyle your clipboard selections\n\n" <>
-                           "Available commands\n" <>
-                           "daemon: Spawn the daemon that will listen to selections\n" <>
-                           "print:  Display all selections history\n" <>
-                           "clear:  Clear history\n" <>
-                           "help:   Display this message\n"
+    HELP     -> putText $ "greenclip v2.1 -- Recyle your clipboard selections\n\n" <>
+                          "Available commands\n" <>
+                          "daemon: Spawn the daemon that will listen to selections\n" <>
+                          "print:  Display all selections history\n" <>
+                          "clear:  Clear history\n" <>
+                          "help:   Display this message\n"
 
 main :: IO ()
 main = do
   displayPresent <- lookupEnv "DISPLAY"
   case displayPresent of
-    Nothing -> putStrLn "X display not available. Please start Xorg before running greenclip"
-    _       -> getArgs >>= run .parseArgs
+    Nothing -> putText "X display not available. Please start Xorg before running greenclip"
+    _       -> getArgs >>= run . parseArgs . fmap toS
